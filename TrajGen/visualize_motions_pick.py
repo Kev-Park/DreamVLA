de5f8bb923d8df3205077365f1ba328042ac20b9
Kev-Park <kevy0403@gmail.com>
2026-02-19 00:44:11 -0500

    fix link to .urdf

--- TrajGen/visualize_motions_pick.py
+++ TrajGen/visualize_motions_pick.py
@@ -183,11 +183,11 @@ def main():
 
     """CHANGED FORMAT FOR COMMAND INPUT TESTING"""
     joint_names = ['left_hip_pitch_joint', 'left_hip_roll_joint', 'left_hip_yaw_joint', 'left_knee_joint', 'left_ankle_pitch_joint', 'left_ankle_roll_joint', 'right_hip_pitch_joint', 'right_hip_roll_joint', 'right_hip_yaw_joint', 'right_knee_joint', 'right_ankle_pitch_joint', 'right_ankle_roll_joint', 'waist_yaw_joint', 'left_shoulder_pitch_joint', 'left_shoulder_roll_joint', 'left_shoulder_yaw_joint', 'left_elbow_joint', 'left_wrist_roll_joint', 'left_wrist_pitch_joint', 'left_wrist_yaw_joint', 'right_shoulder_pitch_joint', 'right_shoulder_roll_joint', 'right_shoulder_yaw_joint', 'right_elbow_joint', 'right_wrist_roll_joint', 'right_wrist_pitch_joint', 'right_wrist_yaw_joint']
-    urdf_path = '../../Training/HumanoidVerse/humanoidverse/data/robots/g1/g1_27dof.urdf'
+    urdf_path = '../Training/HumanoidVerse/humanoidverse/data/robots/g1/g1_27dof.urdf'
     pk2_robot = pk2.build_chain_from_urdf(open(urdf_path).read())
 
     #joint_names = ['left_hip_pitch_joint', 'left_hip_roll_joint', 'left_hip_yaw_joint', 'left_knee_joint', 'left_ankle_pitch_joint', 'left_ankle_roll_joint', 'right_hip_pitch_joint', 'right_hip_roll_joint', 'right_hip_yaw_joint', 'right_knee_joint', 'right_ankle_pitch_joint', 'right_ankle_roll_joint','waist_pitch_joint', 'waist_roll_joint', 'waist_yaw_joint', 'left_shoulder_pitch_joint', 'left_shoulder_roll_joint', 'left_shoulder_yaw_joint', 'left_elbow_joint', 'left_wrist_roll_joint', 'left_wrist_pitch_joint', 'left_wrist_yaw_joint', 'right_shoulder_pitch_joint', 'right_shoulder_roll_joint', 'right_shoulder_yaw_joint', 'right_elbow_joint', 'right_wrist_roll_joint', 'right_wrist_pitch_joint', 'right_wrist_yaw_joint']
-    #urdf_path = '../../HumanoidVerse/humanoidverse/data/robots/g1/g1_paddle_hand_rigid.urdf'
+    #urdf_path = '../Training/HumanoidVerse/humanoidverse/data/robots/g1/g1_paddle_hand_rigid.urdf'
     #pk2_robot = pk2.build_chain_from_urdf(open(urdf_path).read())
     
     keypts = get_keypts(torch.tensor(joints), joint_names , pk2_robot=pk2_robot) 
