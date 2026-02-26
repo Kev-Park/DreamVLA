479cee8d4126e7e96c56b1af538bc88a3fb5f76e
Kev-Park <kevy0403@gmail.com>
2026-02-26 00:33:13 -0500

    square speeds

--- TrajGen/sample/Pick_sim1/refine_motions.py
+++ TrajGen/sample/Pick_sim1/refine_motions.py
@@ -97,7 +97,7 @@ def compute_cost(joint_angles, trans, quats, offset_x=OFFSET_X, offset_z=OFFSET_
 
             # [ABS] Penalize wrist speed changes (acceleration) and high speed relative to own trajectory
             cost2[1:-1] += torch.abs(rel_dists[1:] - rel_dists[:-1])**2  # smoothness of own speed
-            cost2[1:] += torch.abs(rel_dists)                          # L1 speed magnitude
+            cost2[1:] += torch.abs(rel_dists)**2                          # L1 speed magnitude
             cost2[1:] += 10*rel_dists**2                               # L2 speed magnitude (dominates large speeds)
 
             # [REF] Laziness: penalize deviation from rest pose, decaying toward grab_idx
