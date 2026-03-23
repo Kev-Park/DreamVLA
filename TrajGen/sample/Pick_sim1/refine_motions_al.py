3b308830722140b8bc4a9b755caf4c6f69f702ba
Kev-Park <kevy0403@gmail.com>
2026-03-23 00:19:42 -0400

    temp remove distance cost

--- TrajGen/sample/Pick_sim1/refine_motions_al.py
+++ TrajGen/sample/Pick_sim1/refine_motions_al.py
@@ -463,7 +463,7 @@ def compute_cost(joint_angles, trans, quats, offset_x=OFFSET_X, offset_z=OFFSET_
                 taper_linear = torch.clamp((float(taper_end) - frame_ids[taper_mask]) / float(taper_denom), min=0.0, max=1.0)
                 taper[taper_mask] = taper_linear ** 2
 
-                HAND_APPROACH_SOFT_WEIGHT = 2000.0
+                HAND_APPROACH_SOFT_WEIGHT = 2800.0*0
                 cost2[1:grab_idx] += HAND_APPROACH_SOFT_WEIGHT * taper * approach_pen
         else:
             continue
