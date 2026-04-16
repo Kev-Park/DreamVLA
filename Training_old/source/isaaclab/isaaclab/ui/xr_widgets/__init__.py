fece6a6507c0c005915c2bf8845fe71591cc6e0b
Kev-Park <kevy0403@gmail.com>
2026-04-16 12:12:44 -0400

    remove TrainingOld

--- Training_old/source/isaaclab/isaaclab/ui/xr_widgets/__init__.py
+++
@@ -1,5 +0,0 @@
-# Copyright (c) 2025, The Isaac Lab Project Developers.
-# All rights reserved.
-#
-# SPDX-License-Identifier: BSD-3-Clause
-from .instruction_widget import SimpleTextWidget, show_instruction
